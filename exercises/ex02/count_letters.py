fcb548593adaf89a501e6a3d2dec33f583481b0f
Sarah Brown <sarahchu@live.unc.edu>
2021-09-08 22:11:14 -0400

    progress on ex02

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -6,3 +6,12 @@ letter: str = input("What letter do you want to search for?:")
 word: str = input("Enter a word:")
 
 i: int = 0
+count: int = 0
+
+while i < len(word):
+    if word[i] == letter:
+        count = count + 1
+    i = i + 1
+
+print("Count: ", count)
+ 
