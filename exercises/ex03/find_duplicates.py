fbdcc6abe8a2e158645c072856a6e7ad55234775
Sarah Brown <sarahchu@live.unc.edu>
2021-09-13 23:45:05 -0400

    progress on ex03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,17 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730228276"
+
+word: str = input("Enter a word: ")
+
+i: int = 0
+
+while i < len(word):
+    character1: str = str(word[i])
+    print(character1)
+    j: int = i + 1
+    character2: str = str(word[j])
+    print(character2)
+    i = i + 1
+    j = j + 1
+   
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,14 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730228276"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+depth: int = int(input("Depth: "))
+
+i: int = 0
+while i < depth:
+    print(TREE)
+    i = i + 1
+
