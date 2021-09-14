b7154dc34b1e732ed398e783a4b4fc5690460a70
Sarah Brown <sarahchu@live.unc.edu>
2021-09-14 13:32:35 -0400

    progress on ex03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -6,14 +6,17 @@ word: str = input("Enter a word: ")
 dup: bool = False
 
 i: int = 0
+j: int = 0
+
 while i < len(word):
     character: str = word[i]
-    j: int = i + 1
+    j = j + 1
     while j < len(word):
         if character == word[j]:
-            dup: bool = True
-
-        j = j + 1
+            j = j + 1
+            dup = True
+        else:
+            j = j + 1
     i = i + 1
         
 print(dup)
