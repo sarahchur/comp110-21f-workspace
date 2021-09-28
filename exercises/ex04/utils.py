01fde72857993c0c77c5ecaee761ae7790d02c9b
Sarah Brown <sarahchu@live.unc.edu>
2021-09-27 20:05:22 -0400

    progress on ex04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,13 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730228276"
 
 
-# TODO: Implement your functions here.
+def all(x: list[int], y: int) -> bool:
+    """Return boolean values when comparing an integer to my list."""
+    i: int = 0
+    while i < len(x):
+        if y == x[i]:
+            return True
+        i = i + 1
+    return False
