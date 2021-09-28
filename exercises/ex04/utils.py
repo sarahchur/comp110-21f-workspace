24ddca6961003dee8616a8b5241ff49117d7492f
Sarah Brown <sarahchu@live.unc.edu>
2021-09-28 16:41:43 -0400

    progress on ex04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -7,10 +7,10 @@ def all(x: list[int], y: int) -> bool:
     """Return boolean values when comparing an integer to my list."""
     i: int = 0
     while i < len(x):
-        if y == x[i]:
-            return True
+        if y != x[i]:
+            return False
         i = i + 1
-    return False
+    return True
 
 
 def is_equal(x: list[int], y: list[int]) -> bool:
