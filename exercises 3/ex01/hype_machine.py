aa7b1c31a0f912683bd55c4271430e187d957d26
Sarah Brown <sarahchu@live.unc.edu>
2021-10-05 15:24:48 -0400

    progress on ex05

--- exercises 3/ex01/hype_machine.py
+++
@@ -1,8 +0,0 @@
-"""Exercise 01: Hype Machine is all about hyping myself up!"""
-__author__ = "730228276"
-
-name: str = input("What is your name? ")
-
-print(name + ", you are so great! ")
-print("You are doing great in school, " + name + ".")
-print("Yes, this is true, " + name + ", keep up the good work! ")
