511257e2cf13bd1c4542eb033e9a1ce27fe4a0af
Sarah Brown <sarahchu@live.unc.edu>
2021-10-05 15:16:41 -0400

    progress on ex05

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -6,13 +6,28 @@
 __author__ = "730228276"
 
 
-def test_only_evens() -> None:
-    """Test only_evens to account for only even numbers in the list"""
+from exercises.ex05.utils import only_evens, sub
+
+
+def test_only_evens1() -> None:
+    """Test only_evens to account for increasing numbers in a list."""
     x: list[int] = [1, 2, 3, 4]
-    assert sum(x) == [2, 4]
+    assert only_evens(x) == [2, 4]
+
+
+def test_only_evens2() -> None:
+    """Test only_evens to account for only odds in a lists."""
+    x: list[int] = [1, 3, 5, 7]
+    assert only_evens(x) == []
+
+
+def test_only_evens3() -> None:
+    """Test only_evens to account for a list of all evens."""
+    x: list[int] = [2, 4, 6, 8, 10]
+    assert only_evens(x) == [2, 4, 6, 8, 10]
 
 
 def test_sub() -> None:
-    """Test sub to include only numbers specified by indicies"""
+    """Test sub to include only numbers specified by indicies."""
     a_list: list[int] = [10, 20, 30, 40]
-    assert sum(a_list) == [20, 30]
+    assert sub(a_list, 1, 3) == [20, 30]
