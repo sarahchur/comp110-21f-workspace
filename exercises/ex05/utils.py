fa88bd7f2330e3afd8f7acd43755cd069d2fc54c
Sarah Brown <sarahchu@live.unc.edu>
2021-10-04 23:27:42 -0400

    progress on ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,31 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730228276"
 
 # Define your functions below
+
+
+def only_evens(x: list[int]) -> list[int]:
+    """Return only even numbers from the list we entered."""
+    i: int = 0
+    evens: list[int] = list()
+    while i < len(x):
+        if x[i] % 2 == 0:
+            evens.append(x[i])
+        i += 1
+    return evens
+
+
+def sub(a_list: list[int], x: int, y: int) -> list[int]:
+    """Return a subset of a_list between the 2 values."""
+    i: int = 0
+    
+
+
+
+    
+
+
+
+
+
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,13 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730228276"
+
+
+def test_only_evens() -> None:
+    """Test only_evens to account for only even numbers in the list"""
+    x: list[int] = [1, 2, 3, 4]
+    assert sum(x) == [2, 4]
+
+
+def test_sub() -> None:
