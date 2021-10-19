40dc108d2dc9ac7b4e99c5a91f562ed93c52ca2d
Sarah Brown <sarahchu@live.unc.edu>
2021-10-19 13:32:12 -0400

    progress on ex06

--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -8,33 +8,3 @@ __author__ = "730228276"
 from exercises.ex06.dictionaries import invert
 
 
-def test_invert() -> None:
-    """Test with one key and one value in dictionary."""
-    x: dict[str, str] = {'a': 'b'}
-    assert invert(x) == {'b': 'a'}
-
-
-
-
-
-
-
-
-
-
-# def test_invert1() -> None: 
-#     """Invert a dictionary with one key."""
-#     x: dict[str, str] = {'a': 'b'}
-#     assert invert(x) == {'b': 'a'}
-
-
-# def test_invert2() -> None:
-#     """Invert a dictionary with three keys."""
-#     x: dict[str, str] = {'a': 'b', 'c': 'd', 'e': 'f'}
-#     assert invert(x) == {'b': 'a', 'd': 'c', 'f': 'e'}
-
-
-# def test_invert() -> None:
-#     """Invert a dictioary with names."""
-#     x: dict[str, str] = {'Sarah': 'Haynes'}
-#     assert invert(x) == {'Haynes': 'Sarah'}
